94368a025ad304c8b9805415fdd666a2f327c733
AISoftMaster <ormonkenenbayev@gmail.com>
2021-12-21 16:16:05 +0600

    create index.html and everything to do it workable

--- todo_list/webapp/views.py
+++ todo_list/webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+from webapp.models import Task
+
+
+def task_view(request):
+    tasks = Task.objects.all().order_by("date_deadline")
+    return render(request, 'index.html', {"tasks": tasks})
