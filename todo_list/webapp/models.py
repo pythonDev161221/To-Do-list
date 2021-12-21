7e0e123f86905c29999188883493e29eecfa5843
AISoftMaster <ormonkenenbayev@gmail.com>
2021-12-21 15:57:50 +0600

    renamed in models.py Task

--- todo_list/webapp/models.py
+++ todo_list/webapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=200, null=False, blank=False, verbose_name="Заголовок")
+    task = models.CharField(max_length=200, null=False, blank=False, verbose_name="Заголовок")
     status = models.CharField(max_length=200, null=False, blank=False, verbose_name="Статус", default="new")
-    date_done = models.DateField(null=True, blank=True, default=None)
+    date_deadline = models.DateField(null=True, blank=True, default=None)
 
