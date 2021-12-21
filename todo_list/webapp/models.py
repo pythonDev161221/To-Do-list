5cdafc06ff3dccccb71471a17130838f6c2b5250
AISoftMaster <ormonkenenbayev@gmail.com>
2021-12-21 16:22:07 +0600

    add status_choices to models.py Task status

--- todo_list/webapp/models.py
+++ todo_list/webapp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Task(models.Model):
     task = models.CharField(max_length=200, null=False, blank=False, verbose_name="Заголовок")
-    status = models.CharField(max_length=200, null=False, blank=False, verbose_name="Статус", default="new")
+    status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+    status = models.CharField(max_length=200, null=False, blank=False, choices=status_choices, verbose_name="Статус", default="new")
     date_deadline = models.DateField(null=True, blank=True, default=None)
 
