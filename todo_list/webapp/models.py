8548bacd13153f2dcd8919c3d96ba41d0021bd3d
AISoftMaster <ormonkenenbayev@gmail.com>
2021-12-21 15:15:18 +0600

    add class Task to models.py

--- todo_list/webapp/models.py
+++ todo_list/webapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name="Заголовок")
+    status = models.CharField(max_length=200, null=False, blank=False, verbose_name="Статус", default="new")
+    date_todo = models.DateField(null=True, blank=True, default=None)
+
