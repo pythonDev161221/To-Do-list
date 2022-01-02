f468226e7c32d557da49c1055132492121f3848f
pythonDev161221 <adverbekreklamov@gmail.com>
2022-01-03 01:45:59 +0600

    change "/" main address to name index in urls

--- todo_list/webapp/urls.py
+++ todo_list/webapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import task_view, detail_view, delete_view, create_view
 
 urlpatterns = [
-    path('', task_view),
+    path('', task_view, name="index"),
     path('task/', detail_view),
     path('delete/', delete_view),
     path('create/', create_view),
