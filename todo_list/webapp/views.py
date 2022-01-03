07270f5f58d876f2200aad316cc393677ece6a03
pythonDev161221 <adverbekreklamov@gmail.com>
2022-01-03 16:58:55 +0600

    add try excepts and if. add mistake catchers

--- todo_list/webapp/views.py
+++ todo_list/webapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+import datetime
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseNotFound, Http404
 
 from webapp.models import Task
 
@@ -9,12 +12,12 @@ def task_view(request):
 
 
 def detail_view(request, pk):
-    task = Task.objects.get(pk=pk)
+    task = get_object_or_404(Task, pk=pk)
     return render(request, 'detail.html', {'task': task})
 
 
 def delete_view(request, pk):
-    task = Task.objects.get(pk=pk)
+    task = get_object_or_404(Task, pk=pk)
     task.delete()
     return redirect("index")
 
@@ -28,6 +31,8 @@ def create_view(request):
         task_text = request.POST.get('task')
         status = request.POST.get('status')
         date_deadline = request.POST.get('date_deadline')
+        if not date_deadline:
+            return HttpResponseNotFound("date mistake has been occurred, it is need to input date")
         text = request.POST.get('text')
         task = Task(task=task_text, status=status, date_deadline=date_deadline, text=text)
         task.save()
