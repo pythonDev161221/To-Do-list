63586e86a7e42590dba78d85504357acc8c25904
pythonDev161221 <adverbekreklamov@gmail.com>
2022-01-05 19:53:45 +0600

    fill form.html with copied from create.html

---
+++ todo_list/webapp/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+#
+# class ArticleForm(forms.Form):
+#     task = forms.CharField(max_length=200, required=True, label="Task")
+#     https://docs.djangoproject.com/en/4.0/ref/forms/widgets/
