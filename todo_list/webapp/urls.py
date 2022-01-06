813a86c543cf71ee18e7c876b01f260eeea562e5
pythonDev161221 <adverbekreklamov@gmail.com>
2022-01-06 18:15:52 +0600

    create detail.html and make that workable

--- todo_list/webapp/urls.py
+++ todo_list/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import task_view, detail_view, delete_view, create_view
+from .views import task_view, detail_view, delete_view, create_view, update_view
 
 urlpatterns = [
     path('', task_view, name="index"),
     path('task/<int:pk>', detail_view, name="task_detail"),
     path('delete/<int:pk>', delete_view, name="delete"),
     path('create/', create_view, name="task_create"),
+    path('update/<int:pk>', update_view, name="task_update"),
 ]
